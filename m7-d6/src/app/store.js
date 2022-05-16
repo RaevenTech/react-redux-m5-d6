d7f53e54914677f36c94f3c3e8177f0f4624d1eb
RaevenTech <100418770+RaevenTech@users.noreply.github.com>
2022-05-16 18:43:34 +0100

    persist storage

--- m7-d6/src/app/store.js
+++ m7-d6/src/app/store.js
@@ -1,10 +1,21 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+
 import favouritesReducer from "../slices/favourites/favouritesSlice";
 import resultsReducer from "../slices/results/resultsSlice";
 
+const reducers = combineReducers({
+    favourites: favouritesReducer,
+    results: resultsReducer,
+});
+const persistConfig = {
+    key: "root",
+    storage: storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, reducers);
+
 export default configureStore({
-    reducer: {
-        favourites: favouritesReducer,
-        results: resultsReducer,
-    },
+    reducer: persistedReducer,
 });
--- m7-d6/src/index.js
+++ m7-d6/src/index.js
@@ -1,14 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Provider } from "react-redux";
+import configureStore from "./app/store";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+let persistor = persistStore(configureStore);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <Provider store={configureStore}>
+        <PersistGate persistor={persistor}>
+            <App />
+        </PersistGate>
+    </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
